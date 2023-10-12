e591e99e585f09828d8e930b596c584383383b47
Divyaranjan Pradhan <divyaranjan619@gmail.com>
2023-10-12 14:04:47 +0530

    Youtube Clone

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = "AIzaSyBAMQ1OaY3-KlgEEbhqv2a13DBozrxT_n0";
+const GOOGLE_API_KEY = "Put the personalised Google API Key here";
 
 export const YOUTUBE_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+ GOOGLE_API_KEY;
 

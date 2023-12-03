227e9c306e5a82abf5586a418f48450bf55155a7
Divyaranjan Pradhan <divyaranjan619@gmail.com>
2023-12-03 06:32:38 +0530

    Responsive website update

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,5 +2,5 @@ const GOOGLE_API_KEY = "AIzaSyBAMQ1OaY3-KlgEEbhqv2a13DBozrxT_n0";
 
 export const YOUTUBE_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+ GOOGLE_API_KEY;
 
-export const YOUTUBE_SEARCH_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+export const YOUTUBE_SEARCH_API = "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
  
